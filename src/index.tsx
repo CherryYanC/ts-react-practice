b7fc32d813dc2c8d6bc8f628cc2c508596be6939
yancheng <luck_yan_0801@163.com>
2021-12-27 15:28:33 +0800

    fix: 项目整理

--- src/index.tsx
+++ src/index.tsx
@@ -7,19 +7,11 @@ import { store } from '@/stores/index'
 import Test from './ErrorBoundary'
 // import { fibonacci, memoizedFibonacci } from './utils'
 // import { fibonacci } from './utils'
-import VirtualList from './pages/virtualList'
+import VirtualList from '@/packages/virtualList'
 
 configure({ enforceActions: 'always' }) // 开启严格模式
 
 class App extends React.Component {
-  // componentDidMount() {
-  //   console.time('???')
-  //   for (let i = 0; i < 100; i++) fibonacci(30) // ~5000ms
-  //   console.timeEnd('???')
-  //   console.time('???12312')
-  //   for (let i = 0; i < 100; i++) memoizedFibonacci(30) // ~50ms
-  //   console.timeEnd('???12312')
-  // }
   render() {
     return (
       <Provider {...store}>
---
+++ src/pages/test/index.tsx
@@ -0,0 +1,7 @@
+import React, { FC } from 'react'
+
+const Test: FC = () => {
+  return <>hello test</>
+}
+
+export default Test
